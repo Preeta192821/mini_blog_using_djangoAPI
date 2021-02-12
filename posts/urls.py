99b710544a0006cb1ee59f6d4af29dda1df21160
Samir <Saitwalsamir@gmail.com>
2021-02-12 10:44:08 +0530

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,10 +5,7 @@ from . import views
 urlpatterns =[
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-
-
-
-
+    path('create/', views.PostCreateView.as_view()),
 
 
 ]
